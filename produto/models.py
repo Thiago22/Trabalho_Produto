f6c30c7df7456d36bfe2768fdc56b646895492b9
Thiago22 <thiago5046@hotmail.com>
2014-11-12 23:37:02 +0000

    Teste

---
+++ produto/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Produto (models.Model):
+    produto = models.CharField(max_length=70)
+    fornecedor = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=25)
+    
