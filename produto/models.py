02b73d5dc8d4e1f07768e89bebde758fa5742bb7
Thiago22 <thiago5046@hotmail.com>
2014-11-12 23:43:12 +0000

    Alterando

--- produto/models.py
+++ produto/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Produto (models.Model):
     produto = models.CharField(max_length=70)
     fornecedor = models.CharField(max_length=100)
-    email = models.EmailField(),
+    email = models.EmailField()
     telefone = models.CharField(max_length=25)
     
